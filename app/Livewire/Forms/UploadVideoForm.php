427e31428f87a5e00feafca4ff351e0d7bc5532a
Matilde Enevoldsen <matildeene@outlook.com>
2024-01-27 11:30:42 +0000

    episode 12

---
+++ app/Livewire/Forms/UploadVideoForm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+
+class UploadVideoForm extends Form
+{
+    #[Validate('required')]
+    public string $title;
+    #[Validate('required')]
+    public string $description;
+    #[Validate('required')]
+    public array $tags;
+    #[Validate('required')]
+    public $thumbnail_path;
+}
